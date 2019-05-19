6a9dd0c07afa4e07e8196dba2ca078758c833f03
Jesse <47982943+JesseTimonen@users.noreply.github.com>
2019-05-19 15:27:16 +0300

    Delete __init__.lua

--- TeamOrganizer/Scripts/Utility/Colorpicker/Turbine/__init__.lua
+++
@@ -1,4 +0,0 @@
-local importPath = getfenv(1)._.Name;
-local commonPath = string.gsub(importPath, "%.Turbine$", "");
-import (commonPath .. ".Turbine.Type");
-import (commonPath .. ".Turbine.Class");
